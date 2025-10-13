c8492e739c0fff8fead04aaaa45c52d5d38640fd
Ryan Attard <ryanattard@gmail.com>
2025-10-13 10:52:02 +0200

    Repositories + DI

---
+++ DataAccess/Repositories/BooksRepository.cs
@@ -0,0 +1,75 @@
+﻿using DataAccess.Context;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    //Repository classes which contain raw CRUD operations
+    //C = create
+    //R = read
+    //U = update
+    //D = delete
+
+
+    //IQueryable vs IEnumerable/List/ToList....
+    //IQueryable - it just forms SQL commands (LINQ (at runtime) -> in SQL) and it stops there
+    //IEnumerable - it does what happens in IQueryable and it opens the connection to the database to 
+    //              to execute the prepared SQL command
+
+    public class BooksRepository
+    {
+        public ShoppingCartDbContext _context { get; set; }
+        //when you have a paramter in the constructor it can be injected using DI
+        public BooksRepository(ShoppingCartDbContext context) {
+            _context = context;
+        }
+
+        public IQueryable<Book> Get() { 
+           return _context.Books.OrderBy(x=>x.Title);
+        }
+
+        public Book Get(int id) {
+            return Get().SingleOrDefault(x => x.Id == id);
+        }
+
+        public IQueryable<Book> Get(string keyword)
+        {
+            return Get().Where(x => x.Title.Contains(keyword));
+        }
+
+        public void Add(Book book) { 
+          _context.Books.Add(book); //this saves the data in the (volatile) memory
+          _context.SaveChanges(); //this what actually pushes the data into the database (the persistance data layer)
+        }
+
+        public void Update(Book book) {
+            var originalBook = Get(book.Id);
+            if (originalBook != null)
+            {
+                originalBook.Title = book.Title;
+                originalBook.Price = book.Price;
+                originalBook.CategoryFK = book.CategoryFK;
+                originalBook.Stock = book.Stock;
+
+                _context.SaveChanges();
+            }
+        }
+
+        public void Delete(Book b) { 
+            _context.Books.Remove(b);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id) {
+            var originalBook = Get(id);
+            if (originalBook != null) 
+                    Delete(originalBook);
+        }
+
+
+    }
+}
---
+++ Presentation/Controllers/BooksController.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess.Repositories;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    //Controllers: actual business logic
+    //             logic that manages the data 
+    //             logic that validates the data
+    //             computations 
+    //             managing views/pages - passing data to and from pages
+    //             managing the User data
+    //             error handling
+    //             redirections 
+    //             management of files
+    //             access management
+    public class BooksController : Controller
+    {
+        
+        //Contructor Injection (one of the ways how you can apply DI)
+        private BooksRepository _booksRepository;
+        public BooksController(BooksRepository booksRepository) {
+            _booksRepository = booksRepository;
+        }
+
+        [HttpGet] //Loads the page (with empty input controls)
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] //Handles the submission form
+        public IActionResult Create(Book b)
+        {
+            _booksRepository.Add(b);
+            return View();
+        }
+       /* public IActionResult Delete(int id)
+        {
+            BooksRepository booksRepository = new BooksRepository(null);
+        }
+        public IActionResult Details(int id)
+        {
+            BooksRepository booksRepository = new BooksRepository(null);
+
+        }*/
+    }
+}
